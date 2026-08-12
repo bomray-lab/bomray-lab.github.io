f20ae3e5ac47481ed5b78c27961f2755c701fcda
nhn <hnzzang@eduverse.ai>
2026-08-12 18:06:22 +0900

    add 사이트맵 설정 (도메인 주소 변경)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://bomray-lab.github.io',
+  site: 'https://bomraylab.com',
   trailingSlash: 'always',
   integrations: [sitemap()],
 });
